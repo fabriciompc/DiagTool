f095a359aaac6b3b6f84cea838de2a2c331b10bd
Fabricio Cardoso <fabricio.cardoso@vizir.com.br>
2022-01-05 21:33:26 -0300

    feat: adjust console and add email sender

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System.Management;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
 
 namespace ConsoleApp1
 {
@@ -14,7 +17,7 @@ namespace ConsoleApp1
             GetComponent("Win32_Processor", "Name");
             Console.Write("GPUs:");
             GetComponent("Win32_VideoController", "Name");
-            Console.Write("Ram Memory:");
+            Console.WriteLine("Ram Memory Installed");
             GetComponent("Win32_PhysicalMemory", "Capacity");
             Console.Write("BIOS Brand: ");
             GetComponent("Win32_BIOS", "Manufacturer");
@@ -32,6 +35,49 @@ namespace ConsoleApp1
             GetComponent("Win32_NetworkAdapter", "Name");
             Console.Read();
 
+            try
+            {
+
+                SmtpClient mySmtpClient = new SmtpClient("my.smtp.exampleserver.net");
+
+                // set smtp-client with basicAuthentication
+                mySmtpClient.UseDefaultCredentials = false;
+                System.Net.NetworkCredential basicAuthenticationInfo = new
+                   System.Net.NetworkCredential("username", "password");
+                mySmtpClient.Credentials = basicAuthenticationInfo;
+
+                // add from,to mailaddresses
+                MailAddress from = new MailAddress("test@example.com", "TestFromName");
+                MailAddress to = new MailAddress("test2@example.com", "TestToName");
+                MailMessage myMail = new System.Net.Mail.MailMessage(from, to);
+
+                // add ReplyTo
+                MailAddress replyTo = new MailAddress("reply@example.com");
+                myMail.ReplyToList.Add(replyTo);
+
+                // set subject and encoding
+                myMail.Subject = "Test message";
+                myMail.SubjectEncoding = System.Text.Encoding.UTF8;
+
+                // set body-message and encoding
+                myMail.Body = "<b>Test Mail</b><br>using <b>HTML</b>.";
+                myMail.BodyEncoding = System.Text.Encoding.UTF8;
+                // text or html
+                myMail.IsBodyHtml = true;
+
+                mySmtpClient.Send(myMail);
+            }
+
+            catch (SmtpException ex)
+            {
+                throw new ApplicationException
+                  ("SmtpException has occured: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
 
         }
 
@@ -43,7 +89,18 @@ namespace ConsoleApp1
             foreach (ManagementObject mj in mos.Get())
             {
                 if (Convert.ToString(mj[syntax]) != "")
-                    Console.WriteLine(Convert.ToString(mj[syntax]));
+                {
+                    if (hwclass == "Win32_PhysicalMemory")
+                    {
+                        var value = Convert.ToString(Int64.Parse(Convert.ToString(mj[syntax])) / 1024 / 1024 / 1024);
+                        Console.WriteLine($"Ram Memory Slot: {value} Gb");
+                    }
+                    else
+                    {
+
+                        Console.WriteLine(Convert.ToString(mj[syntax]));
+                    }
+                }
             }
         }
     }
